b23082b4aa49acb253db98d0e34b93355384974f
Shoili Chowdhury <43523898+Shoili06@users.noreply.github.com>
2019-02-15 16:31:32 +0530

    Update first.dart

--- first.dart
+++ first.dart
@@ -1,4 +1,4 @@
-
+import 'package:meta/meta.dart';
 main(){
   MallardDuck mallardduck = MallardDuck();
   RedheadDuck redheadduck = RedheadDuck();
@@ -7,30 +7,22 @@ main(){
   
   mallardduck.quack();
   redheadduck.quack();
-  rubberduck.swim();
-  rubberduck.quack();
-  rubberduck.bubble();
-  mallardduck.fly();
-  woodenduck.quack();
+  //rubberduck.swim();
+  //rubberduck.quack();
+  //rubberduck.bubble();
+  //mallardduck.fly();
+  //woodenduck.quack();
 }
 
-abstract class Duck{
-  FlyBehavior _flybehavior;
+abstract class Duck implements Duck with FlyBehavior,QuackBehavior{
+ /* FlyBehavior _flybehavior;
   QuackBehavior _quackbehavior;
   
-  Duck(QuackBehavior quackbehavior, FlyBehavior fb)
-    : this._flybehavior =fb,
-      this._quackbehavior = quackbehavior;
-  
-  
-  
-  fly(){
-    _flybehavior.fly();
-  }
-  
-  quack(){
-    _quackbehavior.quack();
-  }
+  Duck({
+    @required QuackBehavior quackbehavior, 
+    @required FlyBehavior flybehavior
+    }): this._flybehavior =flybehavior,
+  			this._quackbehavior = quackbehavior;*/
   
   swim(){
     print("Swim swim swim");
@@ -38,19 +30,19 @@ abstract class Duck{
   
 }
 
-class MallardDuck extends Duck{
-  @override
-  fly(){
-    print("Flap flap flap");
-  }
-  
-  @override
-  quack(){
-    print("Quack quack quack");
-  }
+class MallardDuck extends Duck with FlywithWings, QuackQuack{
+ 
 }
+  
+ 
 
-class RedheadDuck extends Duck{
+class RedheadDuck extends Duck with FlywithWings{
+   RedheadDuck()
+     :super(
+    		flybehavior : FlywithWings(),
+    		quackbehavior : QuackQuack()
+  );
+  
   @override
   fly(){
     print("Flap flap flap");
@@ -100,4 +92,19 @@ abstract class Flyable{
 abstract class Quackable{
   quack();
 }
+
+class FlywithWings implements FlyBehavior{
+  @override
+  fly(){
+    print("Flap flap");
+  }
+}
+
+class QuackQuack implements QuackBehavior{
+  @override
+  quack(){
+    print("Quack quack");
+  }
+}
+
 
